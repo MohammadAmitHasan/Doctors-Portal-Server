f4a97bb24a104b3f6fe88804aa663eedc9426da1
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-12 20:06:54 +0600

    Services API created

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// Using MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.get8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const servicesCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Doctors Portal server is up and running')
